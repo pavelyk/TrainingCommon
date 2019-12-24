832a6f8b53d4469c8534c33fc22413caf995989c
Pavel Kozubenko <pkozuben@itron.com>
2019-12-24 14:12:56 -0800

    Initial Commit.

---
+++ Training.Common/IUdpServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace Training.Common
+{
+	public class UdpMessage
+	{
+		public IPEndPoint SrcEndpoint { get; set; }
+		public byte [] Data { get; set; }
+	}
+
+	public interface IUdpServer
+	{
+		bool IsOpen { get; }
+
+		void Open();
+
+		void Close();
+
+		void Send(IPEndPoint Remote, byte[] Data);
+
+		event Action<UdpMessage> UdpMessageRx;
+	}
+}
